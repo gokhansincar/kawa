b0dcf7f5e72fa40902a8ed503061b7ec9301ec0c
Gökhan Sincar <55689973+gokhansincar@users.noreply.github.com>
2019-11-11 12:31:09 +0100

    add js

---
+++ assets/js/app.js
@@ -0,0 +1,43 @@
+const body = document.querySelector("body");
+const openMenu = document.querySelector("#open-menu");
+const closeMenu = document.querySelector("#close-menu");
+const topMenu = document.querySelector(".top-menu");
+
+openMenu.addEventListener('click', function () {
+    body.classList.add('menu-is-open');
+});
+closeMenu.addEventListener('click', function () {
+    body.classList.remove('menu-is-open');
+});
+
+
+topMenu.addEventListener('click', function (e) {
+
+    let clickedElem = e.target; //console.log(clickedElem);
+    
+    let parents = document.querySelectorAll(".parent");
+    parents.forEach(function(item) {
+        item.classList.remove("open-sub");
+    });
+
+    clickedElem.parentNode.classList.add('open-sub');
+    //console.log(this);
+    
+
+});
+
+document.addEventListener('click', function() {
+
+    let isClickInside = topMenu.contains(event.target); //console.log(isClickInside);
+
+    if (!isClickInside) {
+        let parents = document.querySelectorAll(".parent");
+        parents.forEach(function(item) {
+            item.classList.remove("open-sub");
+        });
+    }
+    
+});
+
+
+
