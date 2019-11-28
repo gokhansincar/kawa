a1dc266c152e6ff41a7f1bcc86adaa47582cc962
Gökhan Sincar <55689973+gokhansincar@users.noreply.github.com>
2019-11-28 14:30:13 +0100

    indententation css and delete style in html

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,29 +17,29 @@ closeMenu.addEventListener('click', function () {
 topMenu.addEventListener('click', function (e) {
 
     let clickedElem = e.target; //console.log(clickedElem);
-    
+
     let parents = document.querySelectorAll(".parent");
-    parents.forEach(function(item) {
+    parents.forEach(function (item) {
         item.classList.remove("open-sub");
     });
 
     clickedElem.parentNode.classList.add('open-sub');
     //console.log(this);
-    
+
 
 });
 
-document.addEventListener('click', function() {
+document.addEventListener('click', function () {
 
     let isClickInside = topMenu.contains(event.target); //console.log(isClickInside);
 
     if (!isClickInside) {
         let parents = document.querySelectorAll(".parent");
-        parents.forEach(function(item) {
+        parents.forEach(function (item) {
             item.classList.remove("open-sub");
         });
     }
-    
+
 });
 
 
